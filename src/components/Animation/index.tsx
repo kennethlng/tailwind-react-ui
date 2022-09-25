152054f253681db13683bdd74edc892c15733ed7
Kenneth Ng <ng.kennethlee@gmail.com>
2022-09-24 20:35:35 -0700

    Remove Animation

--- src/components/Animation/index.tsx
+++
@@ -1,9 +0,0 @@
-import Placeholder from './subComponents/Placeholder';
-import ProgressCircle from './subComponents/ProgressCircle';
-import PulsingCircle from './subComponents/PulsingCircle';
-
-export default {
-  Placeholder,
-  ProgressCircle,
-  PulsingCircle,
-};
